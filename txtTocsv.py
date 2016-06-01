11c35b85347737d3935ab105c65823b48c3125eb
Hong Zu Li <hongzu@ualberta.ca>
2016-06-01 16:42:38 -0600

    add tool

---
+++ txtTocsv.py
@@ -0,0 +1,22 @@
+## this file reads text file and generate csv file. For use, fileLine need to be changed, and of course the directory.
+## Author: Hongzu Li
+## Email: hongzu@ualberta.ca
+import glob, os
+
+def generateCSV(fileName):
+    firstLine="TIME_ABSOLUTE,MDC_ECG_LEAD_I,MDC_ECG_LEAD_II,MDC_ECG_LEAD_III,MDC_ECG_LEAD_AVR,MDC_ECG_LEAD_AVL,MDC_ECG_LEAD_AVF,MDC_ECG_LEAD_V1,MDC_ECG_LEAD_V2,MDC_ECG_LEAD_V3,MDC_ECG_LEAD_V4,MDC_ECG_LEAD_V5,MDC_ECG_LEAD_V6\n"
+    fin=open(fileName,'r')
+    fileName=fileName.split('.')[0]
+    fout=open(fileName+'.csv','w')
+    fout.write(firstLine)
+    for line in fin:
+        fout.write(line)
+    fin.close()
+    fout.close()
+    
+
+
+if __name__ == "__main__":
+    os.chdir("data/")
+    for fileName in glob.glob("*.txt"):
+        generateCSV(fileName)
